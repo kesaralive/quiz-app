4a7d08f91f7cd3865641aeb5aec6f64b0a610eeb
kesaralive <kesaralive@gmail.com>
2022-01-05 06:21:58 +0530

    update: update checkAnswer,  nextQuestion functions & update types

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Navigation from "./components/Nav";
 import { QuestionState, fetchQuizQuestions, Difficulty } from "./API";
 import QuestionCard from "./components/QuestionCard";
 
-type AnswerObject = {
+export type AnswerObject = {
   question: string;
   answer: string;
   correct: boolean;
@@ -38,9 +38,34 @@ const App = () => {
     setLoading(false);
   };
 
-  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!gameOver) {
+      //Users Answer
+      const answer: string = e.currentTarget.value;
+      //Check answer against correct answer
+      const correct: boolean = questions[number].correct_answer === answer;
+      //Add score if answer is correct
+      if (correct) setScore((prev: number) => prev + 1);
+      //Save answer in the array for user answers
+      const answrObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      };
+      setUserAnswers((prev) => [...prev, answrObject]);
+    }
+  };
 
-  const nextQuestion = () => {};
+  const nextQuestion = () => {
+    //Move on to the next question if not the last question
+    const nextQuestion = number + 1;
+    if (nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    } else {
+      setNumber(nextQuestion);
+    }
+  };
   return (
     <div className="App font-Poppins container flex flex-col justify-center">
       <h1 className="text-3xl">Quiz Application</h1>
@@ -65,10 +90,17 @@ const App = () => {
           callback={checkAnswer}
         />
       )}
-
-      <button className="next bg-amber-500 text-white py-2 w-full rounded-md" onClick={nextQuestion}>
-        Next Question
-      </button>
+      {!gameOver &&
+      !loading &&
+      userAnswers.length === number + 1 &&
+      number !== TOTAL_QUESTIONS - 1 ? (
+        <button
+          className="next bg-amber-500 text-white py-2 w-full rounded-md"
+          onClick={nextQuestion}
+        >
+          Next Question
+        </button>
+      ) : null}
     </div>
   );
 };
